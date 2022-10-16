abab8266cb6da77ef19b42670a7aedf8ca940781
jyrajan <49803716+jyrajan@users.noreply.github.com>
2022-10-16 14:21:50 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Demo on 12092022!");
+        System.out.println("Hello World from Demo on 17102022!");
     }
 }
